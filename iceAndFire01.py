103a9ad360bf06076d66c8d6efeca5f9027ee8c2
B-steros <brandonballest@gmail.com>
2025-09-09 16:36:02 +0000

    Exploring how to work with large datasets

---
+++ iceAndFire01.py
@@ -0,0 +1,20 @@
+"""Exploring OpenAPIs with requests"""
+
+import requests
+
+AOIF = "https://www.anapioficeandfire.com/api"
+
+
+def main():
+    gotresp = requests.get(AOIF)
+
+
+    got_dj = gotresp.json()
+
+    print(got_dj)
+
+    print(got_dj.keys())
+
+
+if __name__ == "__main__":
+    main()
