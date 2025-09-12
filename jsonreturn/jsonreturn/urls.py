8e5957c23d839c46dc72904f8cecaa852b43a8e4
B-steros <brandonballest@gmail.com>
2025-09-12 19:04:42 +0000

    returning JSON with django

---
+++ jsonreturn/jsonreturn/urls.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+        path('schools/', views.schools),
+        path('comics/', views.readparams),
+        ]
