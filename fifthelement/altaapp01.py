6c722fbefcc4f8e8ddf1726b1e81eeb11e6395b3
B-steros <brandonballest@gmail.com>
2025-09-12 15:51:41 +0000

    introduction to Django

---
+++ fifthelement/altaapp01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""All in one Django settings to drive Django webserver"""
+
+DEBUG = True
+
+SECRET_KEY = 'Youcandanc3youcanjiv3'
+
+ROOT_URLCONF = __name__
+
+
+urlpatterns = []
+
